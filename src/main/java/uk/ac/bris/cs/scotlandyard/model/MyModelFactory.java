2293a074d54bcf75d7fe15e73434d26f46906576
SebWhipps-Br <nk22674@bristol.ac.uk>
2023-03-15 11:55:56 +0000

    Started MyModelFactory

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
@@ -4,7 +4,10 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.util.stream.Collectors;
 
 /**
  * cw-model
@@ -15,6 +18,48 @@ public final class MyModelFactory implements Factory<Model> {
 	@Nonnull @Override public Model build(GameSetup setup,
 	                                      Player mrX,
 	                                      ImmutableList<Player> detectives) {
+
+
+		new Model(){
+			ImmutableSet<Observer> observerSet;
+
+			@Nonnull
+			@Override
+			public Board getCurrentBoard() {
+				//return new Board.GameState(){}
+				return null;
+			}
+
+			@Override
+			public void registerObserver(@Nonnull Observer observer) {
+				observerSet = new ImmutableSet.Builder<Observer>()
+						.addAll(observerSet)
+						.add(observer)
+						.build();
+			}
+
+			@Override
+			public void unregisterObserver(@Nonnull Observer observer) {
+				for (Observer o : observerSet){
+					if (observer.equals(o)){
+						observerSet = new ImmutableSet.Builder<Observer>()
+								.addAll(observerSet.stream().filter(x -> x != o).collect(Collectors.toList()))
+								.build();
+					}
+				}
+			}
+
+			@Nonnull
+			@Override
+			public ImmutableSet<Observer> getObservers() {
+				return observerSet;
+			}
+
+			@Override
+			public void chooseMove(@Nonnull Move move) {
+
+			}
+		};
 		// TODO
 		throw new RuntimeException("Implement me!");
 	}
